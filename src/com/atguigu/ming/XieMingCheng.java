001f0f65dbc8b8eea582b5571cf88a6aa8224d52
司命 <1491447996@qq.com>
2017-08-17 20:34:02 +0800

    司命修改

--- src/com/atguigu/ming/XieMingCheng.java
+++ src/com/atguigu/ming/XieMingCheng.java
@@ -3,6 +3,10 @@ package com.atguigu.ming;
 public class XieMingCheng {
 	
 	public static void main(String[] args) {
-		System.out.println("我掌握调控人类的命运！");
+		System.out.println("我掌握调控人类的命运！还能改写命运！！！");
+		System.out.println("我掌握调控人类的命运！还能改写命运！！！");
+		System.out.println("我掌握调控人类的命运！还能改写命运！！！");
+		System.out.println("我掌握调控人类的命运！还能改写命运！！！");
+		System.out.println("我掌握调控人类的命运！还能改写命运！！！");
 	}
 }
